d71c1cd91ca71ad0826f363dad2dd865aa44f4a0
Mahad Farooq <mahadshaikh676@gmail.com>
2021-10-22 18:02:26 +0500

    8

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ app.use('/api/userLogin', require('./routes/AppApi/userLogin'));
 app.use('/api/addProduct', require('./routes/api/addProduct'));
 app.use('/api/updateProduct', require('./routes/api/updateProduct'));
 
-if(process.env.NODE_ENV == 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
-// app.use(express.static(path.join(__dirname, 'build')));
+// if(process.env.NODE_ENV == 'production'){
+//     app.use(express.static('client/build'));
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     });
+// }
+app.use(express.static(path.join(__dirname, 'build')));
 
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
